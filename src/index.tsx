21ad538d3e85b1b002c3ec5b1994394b99656a40
Kristina Manatskay <manackaakristina@gmail.com>
2024-04-13 20:04:05 +0300

    fix: fix index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import '@mantine/core/styles.css';
+
+import { App } from './components/App';
+import { MantineProvider } from '@mantine/core';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
 );
